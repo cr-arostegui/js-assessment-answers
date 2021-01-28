442cf5c44decc3239a32d5ba4fc51007d96d92a0
Cristhian Arostegui <cr.arostegui@gmail.com>
2021-01-28 13:55:29 -0400

    Solved async tests

--- app/async.js
+++ app/async.js
@@ -2,10 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(function (resolve, reject) {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
+    return new Promise(function (resolve, reject) {
+      let xmlHttp = new XMLHttpRequest();
+      xmlHttp.onload = function() {
+        let data = JSON.parse(xmlHttp.responseText);
+        let result = [];
 
+        for (const key in data['people']) {
+          result.push(data['people'][key]['name']);
+        }
+
+        result.sort()
+        resolve(result);
+      };
+
+      xmlHttp.open('GET', url);
+      xmlHttp.send();
+    })    
   }
 };
