edd0370a00d04c4b225e4c9b5b1eb2a4735c053e
Cristhian Arostegui <cr.arostegui@gmail.com>
2021-01-28 17:03:43 -0400

    Solved numbers tests

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return 1 & (num >>> (bit - 1));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
+    var result = (num >>> 0).toString(2);
 
+    var zeroPrefix = 8 - result.length;
+
+    for (let index = 0; index < zeroPrefix; index++) {
+      result = '0' + result;
+    }
+
+    return result;
   },
 
   multiply: function(a, b) {
-
+    return parseFloat((a * b).toPrecision(1));
   }
 };
