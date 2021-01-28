5f2ef1e13d683ad02995fb64fae40b5ed38d47f3
Cristhian Arostegui <cr.arostegui@gmail.com>
2021-01-28 18:10:06 -0400

    Solved recursion tests

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,25 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    let result = [];
+    let self = this;
+    let startCounting = true;
+    
+    if (dirName) {
+      startCounting = dirName === data['dir'];
+    }
 
+    if (data['files'].length) {
+      data['files'].forEach(function (value) {
+        if (typeof value === 'object') {
+          result = result.concat(self.listFiles(value, startCounting ? undefined : dirName));
+        } else if (startCounting) {
+          result.push(value);
+        }
+      });
+    }
+
+    return result;
   },
 
   permute: function(arr) {
