5b925453cbcff35564151440dc4903f46aa511f9
Cristhian Arostegui <cr.arostegui@gmail.com>
2021-01-28 17:14:54 -0400

    Solved objects tests

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
+    let result = [];
 
+    for (const key in obj) {
+      if (Object.hasOwnProperty.call(obj, key)) {
+        result.push(key + ': ' + obj[key]);
+      }
+    }
+
+    return result;
   }
 };
