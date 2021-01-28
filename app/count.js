2e7b569bb2d9e5b7c834f083540b99490b8ec247
Cristhian Arostegui <cr.arostegui@gmail.com>
2021-01-28 14:38:23 -0400

    Solved count tests

--- app/count.js
+++ app/count.js
@@ -2,6 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let result = start;
+    console.log(result);
 
+    let timer = setInterval(function() {
+      result++;
+      if (result === end) {
+        clearInterval(timer);
+      }
+
+      console.log(result);
+    }, 100);
+
+    return {
+      cancel: function() {
+        clearInterval(timer);
+      }
+    };
   }
 };
